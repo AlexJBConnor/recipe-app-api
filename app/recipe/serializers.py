31b7e5d464bf51dfb5763565cdb470327027f61d
Alex Connor <aconnor@diligenciagroup.com>
2025-02-03 14:36:21 +0000

    updating viewsets

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -89,6 +89,15 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view"""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
 
 
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """serializer for uploading images to recipes"""
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image':{'required':'True'}}
+
+    
